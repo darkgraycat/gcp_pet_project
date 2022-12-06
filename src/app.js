d8c0c19a2a2593926980762752f2903c6ca3ca88
Pavel Buryanov <jburyanov@gmail.com>
2022-12-06 05:07:45 +0200

    Test autodeploy (#10)
    * 6: try to push image to cloud-builders
    
    * 6: Remove dockerfile, use cloudbuild.yaml from gcp documentation
    
    * 6: upd
    
    * 6: upd
    
    * 6: add health endpoint
    
    * testAutodeploy: test
    
    Co-authored-by: pburyanov <p_buryanov-itssv@kw.com>

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const { app: { port } } = require('./config');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello GCP and real autodeploy now!');
+  res.send('Hello Marishka!');
 });
 
 app.get('health', (req, res) => res.send('OK'));
