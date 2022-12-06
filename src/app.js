07924d4a5cd2c76d3af361fb14c698a6ad3004a7
pburyanov <p_buryanov-itssv@kw.com>
2022-12-06 04:52:58 +0200

    6: add health endpoint

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const { app: { port } } = require('./config');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello GCP and autodeploy! 2');
+  res.send('Hello GCP and real autodeploy now!');
 });
+
+app.get('health', (req, res) => res.send('OK'));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
