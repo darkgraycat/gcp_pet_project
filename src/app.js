6bf973a0afd94edb6acdd7d669a69e39a5f520ef
Pavel Buryanov <jburyanov@gmail.com>
2022-12-06 21:58:29 +0200

    15: Connect DB with Application (#16)
    * 15: install pg, add gitignore
    
    * 15: add connection to DB, add env variables, add route to test
    
    * 15: prelaod dotenv
    
    * 15: refactor, setup basic app infrastructure
    
    Co-authored-by: pburyanov <p_buryanov-itssv@kw.com>

--- src/app.js
+++ src/app.js
@@ -1,14 +1,44 @@
 const express = require('express');
-const { app: { port } } = require('./config');
+const { Client } = require('pg');
+const { app: { port }, db } = require('./config');
 
-const app = express();
+(async (app, db, config) => {
+  const { port } = config;
+  await db.connect();
 
-app.get('/', (req, res) => {
-  res.send('Autodeploy 2');
-});
+  app.get('/', (req, res) => {
+    res.send('Home');
+  });
 
-app.get('health', (req, res) => res.send('OK'));
+  app.get('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const data = await db.query(`select * from users where id = ${id}`);
+      if (!data.rows.length) return res.status(404).send('Not found');
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
-});
+      return res.json(data.rows[0]);
+    } catch ({ message }) {
+      return res.status(500).send(message);
+    }
+  });
+
+  app.get('/health', (req, res) => res.send('OK'));
+
+  const server = app.listen(port, () => {
+    console.info(`Server listening on port ${port}...`);
+  });
+
+  process.on('unhandledRejection', (reason) => {
+    console.error(`Unhandled rejection ${reason}`);
+  });
+
+  ['SIGINT', 'SIGTERM'].forEach((signal) => process.on(signal, () => {
+    db.end();
+    console.info(`Server recieves signal ${signal}`);
+    process.exit(0);
+  }));
+})(
+  express(),
+  new Client(db),
+  { port }
+);
--- src/config.js
+++ src/config.js
@@ -1,7 +1,18 @@
 const app = {
-  port: process.env.PORT || 8080
+  env: process.env.NODE_ENV || 'local',
+  port: process.env.PORT || 8080,
+  request_timeout: Number(process.env.REQUEST_TIMEOUT_MS) || 30000
+};
+
+const db = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
 
 module.exports = {
   app,
+  db,
 };
