ec336a600dc8a6cd824171f9871c6d3c2561655a
pburyanov <p_buryanov-itssv@kw.com>
2022-12-06 20:59:34 +0200

    15: add connection to DB, add env variables, add route to test

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const { app: { port } } = require('./config');
+const { Client } = require('pg');
+
+const { app: { port }, db } = require('./config');
 
 const app = express();
+const client = new Client(db);
 
 app.get('/', (req, res) => {
   res.send('Autodeploy 2');
+});
+
+app.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await client.connect();
+    const user = await client.query(`select * from users where id = ${id}`);
+    await client.end();
+
+    return res.json(user);
+  } catch ({ message }) {
+    return res.status(500).send(message);
+  }
 });
 
 app.get('health', (req, res) => res.send('OK'));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled rejection', reason.stack);
 });
--- src/config.js
+++ src/config.js
@@ -1,7 +1,17 @@
 const app = {
-  port: process.env.PORT || 8080
+  env: process.env.NODE_ENV,
+  port: process.env.PORT
+};
+
+const db = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
 
 module.exports = {
   app,
+  db,
 };
