a7358f9003e09d84ffe56bca22a8b85461122834
pburyanov <p_buryanov-itssv@kw.com>
2022-12-07 04:51:40 +0200

    15: fix scripts, add logs to debug env variables

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const mysql = require('mysql2/promise');
 const { app: { port }, db } = require('./config');
 const routes = require('./routes');
 
+console.warn(JSON.stringify(db));
+
 (async (app, port, db_config) => {
   // connect to database
   const connection = await mysql.createConnection(db_config);
